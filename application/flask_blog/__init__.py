0bfe650054edfbad2dc0f6864326c5b537dde2d3
51kigi <kimuragt@gmail.com>
2018-08-17 00:11:47 +0900

    一通り勉強した

--- application/flask_blog/__init__.py
+++ application/flask_blog/__init__.py
@@ -2,8 +2,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-appa=Flask(__name__)
+appa = Flask(__name__)
 appa.config.from_object('flask_blog.config')
 
-db=SQLAlchemy(appa)
-import flask_blog.views
+db = SQLAlchemy(appa)
+
+from flask_blog.views import views,entries
--- application/flask_blog/config.py
+++ application/flask_blog/config.py
@@ -1,8 +1,8 @@
 #flask_blog/config.py
 
-SQLARCHEMY_DATABASE_URI='sqlite:///flask_blog.db'
-SQLARCHEMY_TRACK_MODIFICATION=True
-DEBUG=True
-SECRET_KEY='secret key'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+DEBUG = True
+SECRET_KEY = 'secret key'
 USERNAME='hoge'
 PASSWORD='hoge'
